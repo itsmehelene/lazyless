1ffb488fc1794fdc991671402bf06ae869d50357
Hélène <0302helene@gmail.com>
2020-05-03 12:10:07 +0200

    new, create, show - done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,19 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/tasks/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+
 def show
   @task = Task.find(params[:id])
 end
@@ -10,6 +23,6 @@ end
   private
 
   def task_params
-    params.require(task).permit(:done)
+    params.require(:task).permit(:done, :name)
   end
 end
